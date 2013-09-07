3e80950c1ce8095656544d73054a096d50dd42ea
Austin Heimark <austin@duckduckgo.com>
2013-09-06 21:32:21 -0400

    Added a few template pages

--- views.py
+++ views.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import (
+    Flask, 
+    render_template,
+    redirect,
+    url_for
+    )
+
 app = Flask(__name__)
 
+
+
 @app.route("/")
-def hello():
-    return 'home.html'
+def home():
+    return render_template('home.html')
+
+@app.route("/dashboard")
+def dashboard():
+    #ensure that the user is logged in
+
+    return render_template('dashboard.html')
+
+
+
+#unauthorized
+@app.errorhandler(401)
+def unauthorized_page(error):
+    return render_template('401.html'), 401
+
+#not found
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run()
