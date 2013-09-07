9422a354c33d40e5951d8865974343c63a214c05
Kevin Leimkuhler <kevleimkuhler@gmail.com>
2013-09-07 17:06:06 -0400

    venmo login

--- views.py
+++ views.py
@@ -8,15 +8,20 @@ from flask import (
     flash,
     abort
     )
+from constants import CONSUMER_KEY, CONSUMER_SECRET, APP_SECRET_KEY
 from flask.ext.sqlalchemy import SQLAlchemy
 import sqlalchemy.orm
 import os, sys, hashlib
 import sqlite3
+<<<<<<< Updated upstream
 import datetime
+=======
+import requests
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 app.debug = True
-app.secret_key = b'Q\x8b\xb3\x0f\xc0`u\xf4\xb8R\x0b\xbe\xe5^\xf5\xea\xe6\xf5\xc0\x8f\x95\xbcR\xdc\x84\xcb\x1fD\x84\x83:\xb6f\xb7\xc0\x19=M\\{\x9a+-a\xa9\xb3\x82B*\x86\t\x91\x11@\x92\x1a\x8d\xd1\x1d\xa4\xff.{\x91\x82\xf6\x10\x9b\x87\xb6\xcb cm\xf7\x81\x91\\k\xe0\x85\x92\xef\xe2\tyi\x1a\x0e\xbc\xbe\x8fN\x93\x84\xf8\xa4\x93\xd0e\x7f\xe7>=RN\x8c\ng8\x9b8\xa3\x1c\xecT#}\x88\xe5\xd2\xb1\x89\x07\xb8\xf2\xb0\x16\xb0h\x91\xe8\x92\x9c\x8aom\x15\n$\x1b\xb09\xb0\x86),\xac58\xeeY\xa6\xc4\x9a\xef\xa41\xc6 '
+app.secret_key = APP_SECRET_KEY
 #app.config.from_object(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -97,7 +102,31 @@ def signup_authenticate():
     #will need to set the session information so that the user is logged in here
     session['username'] = request.form['username']
 
-    #flash('You successfully registered for this website!')
+    # redirect user to venmo login
+    if session.get('venmo_token'):
+        return redirect(url_for('dashboard'))
+    else:
+        return redirect('https://api.venmo.com/oauth/authorize?client_id=%s&scope=make_payments,access_profile&response_type=code' % CONSUMER_KEY)
+
+@app.route('/oauth-authorized')
+def oauth_authorized():
+    AUTHORIZATION_CODE = request.args.get('code')
+    data = {
+        "client_id":CONSUMER_KEY,
+        "client_secret":CONSUMER_SECRET,
+        "code":AUTHORIZATION_CODE
+        }
+    url = "https://api.venmo.com/oauth/access_token"
+    response = requests.post(url, data)
+    response_dict = response.json()
+    app.logger.debug(response_dict)
+    access_token = response_dict.get('access_token')
+    user = response_dict.get('user')
+
+    session['venmo_token'] = access_token
+    session['venmo_username'] = user['username']
+
+    flash("'You were signed in as %s' % user['username']")
     return redirect(url_for('dashboard'))
 
 @app.route("/signin")
